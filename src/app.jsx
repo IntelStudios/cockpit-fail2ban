35dbe0e0a977f1b8f4f599baa3f918cb1557e887
Libor Zoubek <lzoubek@jezzovo.net>
2023-07-03 13:40:04 +0200

    fail2ban initial import

--- src/app.jsx
+++ src/app.jsx
@@ -17,34 +17,90 @@
  * along with Cockpit; If not, see <http://www.gnu.org/licenses/>.
  */
 
-import cockpit from 'cockpit';
-import React from 'react';
+import cockpit from "cockpit";
+import React from "react";
+import {
+    SimpleList,
+    SimpleListItem,
+    SimpleListGroup,
+    Title,
+    EmptyState,
+    CardHeader,
+    Button,
+} from '@patternfly/react-core';
 import { Alert } from "@patternfly/react-core/dist/esm/components/Alert/index.js";
-import { Card, CardBody, CardTitle } from "@patternfly/react-core/dist/esm/components/Card/index.js";
+import {
+    Card,
+    CardBody,
+    CardTitle,
+} from "@patternfly/react-core/dist/esm/components/Card/index.js";
 
 const _ = cockpit.gettext;
+
+export const EmptyStateSpinner = () =>
+    <EmptyState>
+
+        <Title size="lg" headingLevel="h4">
+            Loading
+        </Title>
+    </EmptyState>;
 
 export class Application extends React.Component {
     constructor() {
         super();
-        this.state = { hostname: _("Unknown") };
+        this.state = { status: _("Unknown"), banned: [] };
+    }
 
-        cockpit.file('/etc/hostname').watch(content => {
-            this.setState({ hostname: content.trim() });
+    componentDidMount() {
+        cockpit.spawn(["fail2ban-client", "status"]).done((output) => {
+            this.setState({ status: output.trim() });
+        });
+        this.loadBanned();
+    }
+
+    loadBanned() {
+        this.setState({ banned: [] });
+        cockpit.spawn(["fail2ban-client", "banned"]).done((output) => {
+            this.setState({ banned: JSON.parse(output.trim().replace(/'/g, '"')) });
+        });
+    }
+
+    unban(ip) {
+        console.log('unban');
+        cockpit.spawn(["fail2ban-client", "unban", ip]).done((output) => {
+            this.loadBanned();
         });
     }
 
     render() {
+        const self = this;
+        const groups = this.state.banned
+                .map(function(e, i) {
+                    return Object.keys(e).map(function(key) {
+                        const items = e[key].map(function(item, index) {
+                            return <SimpleListItem key={index} title="Click to unban" onClick={() => self.unban(item)}>{item}</SimpleListItem>;
+                        });
+                        const title = `Jail: ${key}`;
+                        return <SimpleListGroup key={key} title={title}>{items}</SimpleListGroup>;
+                    });
+                }).flat(1);
+
+        const list = groups.length > 0 ? <SimpleList>{groups}</SimpleList> : <EmptyStateSpinner />;
         return (
-            <Card>
-                <CardTitle>Starter Kit</CardTitle>
-                <CardBody>
-                    <Alert
-                        variant="info"
-                        title={ cockpit.format(_("Running on $0"), this.state.hostname) }
-                    />
-                </CardBody>
-            </Card>
+            <div>
+                <Card>
+                    <CardHeader>
+                        <CardTitle>Fail2ban status</CardTitle>
+                        <Button onClick={() => this.loadBanned()}>Reload</Button>
+                    </CardHeader>
+
+                    <CardBody>
+                        <Alert variant="info" title={this.state.status} />
+                        {list}
+                    </CardBody>
+                </Card>
+
+            </div>
         );
     }
 }
